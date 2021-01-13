9edfad00ac25ef6e619e7f1d3cd92425c787dfb2
Artur Zakirov <artur.zakirov@adjust.com>
2021-01-13 17:49:58 +0100

    wltree's delimiter consists of two characters. Its source code heavily relies on this fact, for this we need to check both characters in advance.
    Related with Pull Request #15
    Issue #20

--- sql/wltree.sql
+++ sql/wltree.sql
@@ -9,8 +9,8 @@ SELECT '1::2'::ltree;
 SELECT '1::2::_3'::ltree;
 SELECT '1::2::3 : 4'::ltree;
 SELECT 'foö::bär::ba@ : 4*'::ltree;
-SELECT '::1'::ltree;
-SELECT '::'::ltree;
+SELECT '::1'::ltree; -- fails
+SELECT '::'::ltree; -- fails
 
 SELECT ltree2text('1::2::3::34::sdf');
 SELECT text2ltree('1::2::3::34::sdf');
@@ -94,9 +94,11 @@ SELECT '\*1::\|'::lquery;
 SELECT '\!1::\|'::lquery;
 SELECT '\*1::\{4'::lquery;
 SELECT 'foö::bär::ba\@ \: 4\*'::lquery;
-SELECT '::1'::lquery;
-SELECT '::'::lquery;
+SELECT '::1'::lquery; -- fails
+SELECT '::'::lquery; -- fails
 SELECT '1:2'::lquery;
+SELECT '*:1'::lquery; -- fails
+SELECT '*.1'::lquery; -- fails
 
 SELECT nlevel('1::2::3::4');
 SELECT '1::2'::ltree  < '2::2'::ltree;
